29389f318c1315b91b590a61bb3712b0fa2e273a
DakshxDD <128332958+DakshxDD@users.noreply.github.com>
2024-09-04 22:27:26 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+if (window.performance.navigation.type === 1) {
+    rollDice();
+  }
+   
+
+
+function rollDice(){let randomNumber1= Math.floor(Math.random()*6)+1;
+let randomDiceImage1="./images/dice"+ randomNumber1+ ".png";
+document.querySelector(".img1").setAttribute("src", randomDiceImage1);
+let randomNumber2= Math.floor(Math.random()*6)+1;
+let randomDiceImage2="./images/dice"+ randomNumber2+ ".png";
+document.querySelector(".img2").setAttribute("src", randomDiceImage2);
+
+if(randomNumber1>randomNumber2){
+    document.querySelector("h1").innerHTML="🚩Player 1 Wins!";
+
+}
+else if(randomNumber2>randomNumber1){
+    document.querySelector("h1").innerHTML="Player 2 Wins!🚩";
+}
+else {
+    document.querySelector("h1").innerHTML="It's a Tie!";
+}}
